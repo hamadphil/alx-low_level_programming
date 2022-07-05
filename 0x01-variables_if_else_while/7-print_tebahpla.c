732b2f775842ba3898546c436d8f0c4304c7a700
hamadphil <ahmadadefolajuwonlo@gmail.com>
2022-07-04 21:47:04 -0700

    seventh print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - print reserved alphabet
+ * main - prints reserved alphabet
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
+int i;
 for (i = 122; i > 96; i--)
-}
 {
 putchar(i);
 }
