a22d0e139564d68a798ba218ec1368a552af7a1e
hamadphil <ahmadadefolajuwonlo@gmail.com>
2022-07-04 21:41:38 -0700

    seventh alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print reserved alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+for (i = 122; i > 96; i--)
+}
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
